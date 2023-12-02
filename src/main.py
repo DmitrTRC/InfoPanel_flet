704898f401af590b6cad00bf8597a35d5a5e9928
Dmitry Morozov <9984398@gmail.com>
2023-12-02 17:35:46 +0300

    -> Demo example

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello Flet!")
+import flet as ft
 
 
-if __name__ == '__main__':
-    main()
+def main(page: ft.Page) -> None:
+    def btn_clicked(e):
+        page.add(ft.Text('Button Clicked', color='blue'))
+
+    t = ft.Text(value="Hello, world!", color="red")
+    page.controls.append(t)
+    page.add(
+        ft.Row(controls=[
+            ft.Text("Dmitry"),
+            ft.Text("Arseniy"),
+            ft.Text("Arina")
+        ])
+    )
+
+    page.add(ft.ElevatedButton(text='Press button', on_click=btn_clicked))
+    page.update()
+
+
+ft.app(target=main)
