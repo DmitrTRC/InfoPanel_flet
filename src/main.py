4ef086665e5b860f27a0e2c669d234287c272cb0
Arina Morozova <ehdvevd@gmail.com>
2023-12-07 20:46:25 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -13,17 +13,21 @@ def main(page: ft.Page):
         bgcolor='#1A2520',
         destinations=[
             ft.NavigationRailDestination(
-                icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="First"
+                icon_content=ft.Icon(ft.icons.ACCOUNT_BOX_SHARP),
+                selected_icon_content=ft.Icon(ft.icons.ACCOUNT_BOX_SHARP),
+                label="Info_panel",
             ),
             ft.NavigationRailDestination(
-                icon_content=ft.Icon(ft.icons.BOOKMARK_BORDER),
+                icon_content=ft.Icon(ft.icons.ANALYTICS),
+                selected_icon_content=ft.Icon(ft.icons.ANALYTICS),
+                label="Backlog_api",
+            ),
+
+
+    ft.NavigationRailDestination(
+                icon_content=ft.Icon(ft.icons.AIR),
                 selected_icon_content=ft.Icon(ft.icons.BOOKMARK),
-                label="Second",
-            ),
-            ft.NavigationRailDestination(
-                icon=ft.icons.SETTINGS_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.SETTINGS),
-                label_content=ft.Text("Settings"),
+                label="Weather_forecast",
             ),
         ],
         on_change=lambda e: print("Selected destination:", e.control.selected_index),
@@ -46,40 +50,6 @@ def main(page: ft.Page):
         Implement simple handler ( writing which item clicked )
         
         '''
-    def check_item_clicked(e):
-        e.control.checked = not e.control.checked
-        page.update()
 
-    pb = ft.PopupMenuButton(
-        items=[
-            ft.PopupMenuItem(
-                text="InfoPanel", checked=False, on_click=print('info')
-            ),
-            ft.PopupMenuItem(
-                text="BacklogApi", checked=False, on_click=print('info')
-            ),
-            ft.PopupMenuItem(
-                text="Weather forecast", checked=False, on_click=print('Weather forecast')
-            )
-                ,
-
-            ft.PopupMenuItem(
-                content=ft.Row(
-                    [
-                        ft.Icon(ft.icons.HOURGLASS_TOP_OUTLINED),
-
-                    ]
-                ),
-                on_click=lambda _: print("Button with a custom content clicked!"),
-            ),
-            ft.PopupMenuItem(),  # divider
-            ft.PopupMenuItem(
-                text="Checked item", checked=False, on_click=check_item_clicked
-            ),
-        ]
-    )
-    page.add(pb)
-
-ft.app(target=main)
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
