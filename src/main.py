defe472e2f1a4aa1a2a2385084dc9bd56cf2d622
Dmitry Morozov <9984398@gmail.com>
2023-12-28 22:30:03 +0300

    -> main: Docs added

--- src/main.py
+++ src/main.py
@@ -8,8 +8,41 @@ from user import User
 
 
 class InfoApp(ft.UserControl):
+    """
+    This class represents an application that provides information using a graphical user interface. It inherits from
+    the ft.UserControl class.
+
+    Attributes:
+    - layout (None): The layout of the application.
+    - page (ft.Page): An instance of the Page class representing the page associated with this application.
+    - store (DataStore): An instance of the DataStore class representing the data store used by this application.
+    - boards (list): A list of boards available in the data store.
+    - login_profile_button (ft.PopupMenuItem): A button for logging in.
+    - settings_button (ft.PopupMenuItem): A button for accessing settings.
+    - appbar_items (list): A list of appbar items.
+    - appbar (ft.AppBar): The appbar of the application.
+    """
 
     def __init__(self, page: ft.Page, store: DataStore):
+        """
+
+        :param page: an instance of the Page class
+        :param store: an instance of the DataStore class
+
+        Initializes an instance of the __init__ method.
+
+        This method sets up the layout, page, store, and event handlers for the page. It also initializes the appbar
+        items and sets up the appbar.
+
+        The page parameter is an instance of the Page class. It represents the page that this instance of the method
+        is associated with.
+
+        The store parameter is an instance of the DataStore class. It represents the data store that this instance of
+        the method uses.
+
+        No return value.
+
+        """
         super().__init__()
         self.layout = None
         self.page = page
@@ -56,6 +89,11 @@ class InfoApp(ft.UserControl):
         self.page.update()
 
     def build(self):
+        """
+        Builds the layout of the application.
+
+        :returns: the built layout
+        """
         self.layout = AppLayout(
             self, self.page, self.store,
             tight=True, expand=True, vertical_alignment=ft.TextAlign.START
@@ -63,6 +101,21 @@ class InfoApp(ft.UserControl):
         return self.layout
 
     def initialize(self):
+        """Initializes the software application.
+
+        This method initializes the software application by performing the following steps:
+        - Appending the appbar and layout to the list of views in the `page` object.
+        - Setting the padding of the views to 0.
+        - Updating the `page` object.
+        - Creating a new board with the name 'Task Board' if no boards exist.
+        - Redirecting the page to the home directory ('/').
+
+        Parameters:
+            self: The object instance.
+
+        Returns:
+            None
+        """
         self.page.views.append(
             ft.View(
                 '/',
@@ -82,8 +135,37 @@ class InfoApp(ft.UserControl):
         self.page.go('/')
 
     def login(self, e):
+        """
+        Login method
+
+        This method opens a modal dialog to allow the user to login. It takes an event object as a parameter.
+
+        Parameters:
+        - e: The event object triggered by the login action.
+
+        Returns:
+        None
+
+        Example usage:
+        app.login(e)
+
+        """
 
         def close_dlg(e):
+            """
+
+            Close the Dialog on Login
+
+            This method is used to close the dialog after the user has successfully logged in. It updates the
+            necessary components and displays the user's profile name in the app bar.
+
+            Parameters:
+            - e: The event object that triggered the method (not used)
+
+            Returns:
+            This method does not return anything.
+
+            """
             if user_name.value == '' or password.value == '':
                 user_name.error_text = 'Please provide username'
                 password.error_text = 'Please provide password'
@@ -120,9 +202,30 @@ class InfoApp(ft.UserControl):
         self.page.update()
 
     def settings(self, e):
+        """
+        Open theme colors settings dialog.
+
+        :param e: The event object.
+        """
         from palette import show_theme_colors
 
         def close_dlg(e):
+            """
+            Closes the dialog window.
+
+            Parameters:
+            e (Event): The event object.
+
+            Returns:
+            None
+
+            Example:
+            # Create an instance of the InfoApp class
+            app = InfoApp()
+
+            # Call the close_dlg method passing an event object
+            app.close_dlg(event)
+            """
             dialog.open = False
             self.page.update()
 
@@ -144,6 +247,15 @@ class InfoApp(ft.UserControl):
         self.page.update()
 
     def route_change(self, e):
+        """
+        Changes the route of the page and updates the layout accordingly.
+
+        Args:
+            e: The event object.
+
+        Returns:
+            None
+        """
         t_route = ft.TemplateRoute(self.page.route)
 
         if t_route.match('/'):
@@ -163,8 +275,35 @@ class InfoApp(ft.UserControl):
         self.page.update()
 
     def add_board(self, e):
+        """
+        This method `add_board` is used to add a new board to the system.
+
+        Parameters:
+        - `self`: This parameter refers to the instance of the object calling the method.
+        - `e`: This parameter represents the event object.
+
+        Returns:
+        - None
+
+        Example Usage:
+        ```python
+        add_board(self, e)
+        ```
+
+        """
 
         def close_dlg(e):
+            """
+
+            Close the dialog
+
+            Parameters:
+            - e: Event object
+
+            Returns:
+            None
+
+            """
             if (hasattr(e.control, 'text') and e.control.text != 'Cancel') or (
                     type(e.control) is ft.TextField and e.control.value != ''):
                 self.create_new_board(dialog_text.value)
@@ -172,6 +311,14 @@ class InfoApp(ft.UserControl):
             self.page.update()
 
         def textfield_change(e):
+            """
+            Method to trigger when the text field value changes.
+
+            :param e: The event object representing the change event.
+            :type e: Event
+
+            :return: None
+            """
             if dialog_text.value == '':
                 create_button.disabled = True
             else:
@@ -214,17 +361,42 @@ class InfoApp(ft.UserControl):
         dialog_text.focus()
 
     def create_new_board(self, board_name):
+        """
+        Create a new board with the given name.
+
+        Parameters:
+            board_name (str): The name of the new board.
+
+        Returns:
+            None
+        """
         new_board = Board(self, self.store, board_name)
         self.store.add_board(new_board)
         self.layout.hydrate_all_boards_view()
 
     def delete_board(self, e):
+        """
+        Delete board from the store and update the view layout.
+
+        Parameters:
+        - e: The event object containing the board to delete.
+
+        Return:
+        None
+        """
         self.store.remove_board(e.control.data)
         self.layout.set_all_boards_view()
 
 
 if __name__ == '__main__':
     def main(page: ft.Page):
+        """
+
+        This method is the entry point for the InfoGraph Task Manager application. It takes a single parameter,
+        'page', of type 'ft.Page'.
+
+        :param page: an instance of the 'ft.Page' class representing the application page
+        """
         page.title = 'InfoGraph Task Manager'
         page.padding = 0
         page.theme = ft.theme.Theme(
